b25bed87c78a4798a78b49fe19508915b4fc0883
Helena Sollie <helena.sollie@gmail.com>
2017-10-02 20:16:19 +0200

    Added files for exercices

---
+++ aspnetcore_sollie_helena/Controllers/Book2Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+using aspnetcore_sollie_helena.Models;
+
+namespace aspnetcore_sollie_helena.Controllers
+{
+	[Route("/books2")]
+	public class BookController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var model = new BookDetailModel();
+			model.Author("Deflo");
+			model.Title("Spannend");
+            model.ISBN(12);
+            model.CreatedOn(09/12);
+
+			return View(model);
+		}
+
+	}
+}
--- aspnetcore_sollie_helena/Models/BookDetailModel.cs
+++ aspnetcore_sollie_helena/Models/BookDetailModel.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace aspnetcore_sollie_helena.Models
+                                  
 {
     public class BookDetailModel
     {
-        public BookDetailModel()
-        {
-
+        
             public String Author(get; set;)
             public String Title(get; set;)
-            public String 
+            public String ISBN(get; set;)
+        public String CreatedOn(get; set;)
+
+
+        internal void Author(string v)
+        {
+            throw new NotImplementedException();
         }
+
+        internal void CreatedOn(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ISBN(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        set;)
+            public String CreatedOn(get;
+
+        internal void Title(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        set;); 
     }
 }
